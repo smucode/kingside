0a0492a92482a898a9872bcdda0fb0733acea0b6
Stig Murberg <stimurbe@cisco.com>
2011-12-01 15:52:00 +0100

    wip: fen stuff

---
+++ public/javascripts/ks/board/Fen.js
@@ -0,0 +1,108 @@
+var __ = require('underscore');
+
+var Fen = function(fen) {
+	this.pieces = {};
+	this._parse(fen || 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+};
+
+Fen.prototype.move = function(from, to) {
+	this._updatePiecePlacement(from, to);
+	this._updateActiveColor();
+	this._updateCastling(from);
+};
+
+Fen.prototype.canCastle = function(letter) {
+	return __.include(this.castling, letter);
+};
+
+Fen.prototype._updateCastling = function(from) {
+	if (!this.castling.length) {
+		return false;
+	}
+	switch (from) {
+		case 'a1': this.castling = __.without(this.castling, 'Q'); break;
+		case 'a8': this.castling = __.without(this.castling, 'q'); break;
+		case 'h1': this.castling = __.without(this.castling, 'K'); break;
+		case 'h8': this.castling = __.without(this.castling, 'k'); break;
+		case 'e1': this.castling = __.without(this.castling, 'Q', 'K'); break;
+		case 'e8': this.castling = __.without(this.castling, 'q', 'k'); break;
+	}
+};
+
+Fen.prototype._updatePiecePlacement = function(from, to) {
+	var piece = this.pieces[from];
+	if (!piece) {
+		console.log(this.pieces)
+		throw new Error('You must select a valid piece to move: ' + from);
+	}
+	delete(this.pieces[from]);
+	this.pieces[to] = piece;
+};
+
+Fen.prototype._updateActiveColor = function() {
+	this.activeColor = this.activeColor == 'w' ? 'b' : 'w';
+};
+
+Fen.prototype._parse = function(fen) {
+	var arr = fen.split ? fen.split(' ') : [];
+	if (!arr || arr.length != 6) {
+		throw new Error('A FEN must contain 6 space separated fields: ' + fen);
+	}
+	this._parsePiecePlacement(arr[0]);
+	this._parseActiveColor(arr[1]);
+	this._parseCastling(arr[2]);
+	this._parseEnPassant(arr[3]);
+	this._parseHalfmoveClock(arr[4]);
+	this._parseFullmoveNumber(arr[5]);
+};
+
+Fen.prototype._parsePiecePlacement = function(str) {
+	var arr = str.split('/');
+	if (arr.length != 8) {
+		throw new Error('A FEN must contain 8 ranks separated by /: ' + str);
+	}
+	var files = 'abcdefgh';
+	var ranks = '87654321';
+	__.each(arr, function(rank, rankIdx) {
+		var fileIdx = 0;
+		__.each(rank.split(''), function(p, i) {
+			if (!p.match(/[0-8]/)) {
+				var an = files.charAt(fileIdx) + ranks.charAt(rankIdx);
+				this.pieces[an] = p;
+				fileIdx++;
+			} else {
+				fileIdx += parseInt(p, 10) - 1;
+			}
+		}, this);
+	}, this);
+};
+
+Fen.prototype._parseActiveColor = function(col) {
+	if (col == 'w' || col == 'b') {
+		this.activeColor = col;
+	} else {
+		throw new Exception('Illegal active color: ' + col);
+	}
+};
+
+Fen.prototype._parseCastling = function(str) {
+	if (str.match(/[wqWQ-].*/)) {
+		this.castling = str.split('');
+	} else {
+		throw new Exception('Illegal castling string: ' + str);
+	}
+};
+
+Fen.prototype._parseEnPassant = function(str) {
+	this.enPassant = str;
+};
+
+Fen.prototype._parseHalfmoveClock = function(str) {
+	this.halfmove = str;
+};
+
+Fen.prototype._parseFullmoveNumber = function(str) {
+	this.fullmove = str;
+};
+
+exports.Fen = Fen;
---
+++ public/javascripts/ks/board/test/fen-test.js
@@ -0,0 +1,130 @@
+var vows = require('vows');
+var assert = require('assert');
+
+var __ = require('underscore');
+var Fen = require('../Fen').Fen;
+
+vows.describe('Fen').addBatch({
+	'creating a new Fen object' : {
+		topic : new Fen(),
+
+		'should have 32 pieces' : function(topic) {
+			assert.equal(__.size(topic.pieces), 32);
+		},
+		
+		'a1 should be a white rook' : function(topic) {
+			assert.equal(topic.pieces.a1, 'R');
+		},
+		'a2 should be a white pawn' : function(topic) {
+			assert.equal(topic.pieces.a2, 'P');
+		},
+		'b7 should be a black pawn' : function(topic) {
+			assert.equal(topic.pieces.b7, 'p');
+		},
+		'c8 should be a black bishop' : function(topic) {
+			assert.equal(topic.pieces.c8, 'b');
+		},
+		
+		'the active color should be white' : function(topic) {
+			assert.equal(topic.activeColor, 'w');
+		},
+		
+		'all castling is allowed' : function(topic) {
+			assert.equal(topic.castling.length, 4);
+		},
+		
+		'there is no en passant move' : function(topic) {
+			assert.equal(topic.enPassant, '-');
+		},
+		
+		'the halfmove clock is zero' : function(topic) {
+			assert.equal(topic.halfmove, 0);
+		},
+		
+		'the fullmove number is one' : function(topic) {
+			assert.equal(topic.fullmove, 1);
+		}
+		
+	},
+	'moving a piece' : {
+		topic : new Fen(),
+		
+		'to a empty square should retain the number of pieces': function(topic) {
+			topic.move('a2', 'a3');
+			assert.equal(__.size(topic.pieces), 32);
+		},
+		
+		'to a occupied square should reduce the number of pieces': function(topic) {
+			topic.move('b1', 'b2');
+			assert.equal(__.size(topic.pieces), 31);
+		}
+	},
+	'moving a piece' : {
+		topic : new Fen(),
+		
+		'should change the active color': function(topic) {
+			topic.move('a2', 'a3');
+			assert.equal(topic.activeColor, 'b');
+			
+			topic.move('a3', 'a4');
+			assert.equal(topic.activeColor, 'w');
+		}
+	},
+	'given a full board' : {
+		topic : new Fen(),
+		
+		'all castling should be allowed': function(topic) {
+			assert.isTrue(topic.canCastle('K'));
+			assert.isTrue(topic.canCastle('Q'));
+			assert.isTrue(topic.canCastle('k'));
+			assert.isTrue(topic.canCastle('q'));
+		},
+		'moving rook at a1 should disable white queenside castling': function(topic) {
+			topic.move('a1', 'a2');
+			assert.isTrue(topic.canCastle('K'));
+			assert.isFalse(topic.canCastle('Q'));
+			assert.isTrue(topic.canCastle('k'));
+			assert.isTrue(topic.canCastle('q'));
+		},
+		'moving rook at h1 should disable white kingside castling': function(topic) {
+			topic.move('h1', 'h2');
+			assert.isFalse(topic.canCastle('K'));
+			assert.isFalse(topic.canCastle('Q'));
+			assert.isTrue(topic.canCastle('k'));
+			assert.isTrue(topic.canCastle('q'));
+		},
+		'moving rook at a8 should disable black queenside castling': function(topic) {
+			topic.move('a8', 'a7');
+			assert.isFalse(topic.canCastle('K'));
+			assert.isFalse(topic.canCastle('Q'));
+			assert.isTrue(topic.canCastle('k'));
+			assert.isFalse(topic.canCastle('q'));
+		},
+		'moving rook at h8 should disable black kingside castling': function(topic) {
+			topic.move('h8', 'h7');
+			assert.isFalse(topic.canCastle('K'));
+			assert.isFalse(topic.canCastle('Q'));
+			assert.isFalse(topic.canCastle('k'));
+			assert.isFalse(topic.canCastle('q'));
+		}
+	},
+	'given a full board' : {
+		topic : new Fen(),
+		
+		'moving white king should disable all white castling': function(topic) {
+			topic.move('e1', 'e2');
+			assert.isFalse(topic.canCastle('K'));
+			assert.isFalse(topic.canCastle('Q'));
+			assert.isTrue(topic.canCastle('k'));
+			assert.isTrue(topic.canCastle('q'));
+		},
+		'moving black king should disable all black castling': function(topic) {
+			topic.move('e8', 'e7');
+			assert.isFalse(topic.canCastle('K'));
+			assert.isFalse(topic.canCastle('Q'));
+			assert.isFalse(topic.canCastle('k'));
+			assert.isFalse(topic.canCastle('q'));
+		}
+	}
+
+}).export(module);
