9055735655e6f93c71b97aa1818c7a7898e48e76
Stig Murberg <stimurbe@cisco.com>
2011-12-03 17:54:17 +0100

    removed rails stuff

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Kingside::Application.config.secret_token = 'e6bed4dd3ec0d0663df8a71642375fd8d0f16c922766bdc654f422d19e4d7805a8f2e504edd1871198de767ae5ae3d7785372e705dd8e800db991ee2af0cd2a1'
