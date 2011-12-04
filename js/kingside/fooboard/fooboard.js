b15f20d8e651e22a5351af57e263e21493d75c61
Stig Murberg <stimurbe@cisco.com>
2011-12-04 11:54:22 +0100

    renames

--- js/kingside/fooboard/fooboard.js
+++ js/kingside/fooboard/fooboard.js
@@ -1,21 +1,21 @@
 var __ = require('underscore');
 
-var Board = function(pieces) {
+var FooBoard = function(pieces) {
 	this.pieces = pieces || {};
 	this.files = 'xabcdefgh'.split('');
 	this.ranks = '87654321x'.split('');
 	this._create();
 };
 
-Board.prototype.render = function(target) {
+FooBoard.prototype.render = function(target) {
 	target.appendChild(this.table);
 };
 
-Board.prototype.move = function(from, to) {};
+FooBoard.prototype.move = function(from, to) {};
 
-Board.prototype.remove = function(pos) {};
+FooBoard.prototype.remove = function(pos) {};
 
-Board.prototype._create = function() {
+FooBoard.prototype._create = function() {
 	this.table = this._c('table');
 	this.table.className = 'foo-board';
 	__.each(this.ranks, function(rank) {
@@ -29,7 +29,7 @@ Board.prototype._create = function() {
 	}, this);
 };
 
-Board.prototype._formatCell = function(cell, file, rank) {
+FooBoard.prototype._formatCell = function(cell, file, rank) {
 	if (file == 'x' || rank == 'x') {
 		cell.className = 'legend';
 		cell.innerHTML = (file != 'x' ? file : (rank != 'x' ? rank : '')).toUpperCase();
@@ -39,26 +39,26 @@ Board.prototype._formatCell = function(cell, file, rank) {
 	} 
 };
 
-Board.prototype._setImage = function(cell, file, rank) {
+FooBoard.prototype._setImage = function(cell, file, rank) {
 	var p = this.pieces[file + rank];
 	var type = p ? this._imgMap[p] : 'x';
 	cell.innerHTML = '<img src="img/' + type +'.png" />';
 };
 
-Board.prototype._setClassName = function(cell, file, rank) {
+FooBoard.prototype._setClassName = function(cell, file, rank) {
 	var fileIdx = (__.indexOf(this.files, file) % 2);
 	cell.className = fileIdx - (rank % 2) ? 'light' : 'dark';
 };
 
-Board.prototype._c = function(tagName) {
+FooBoard.prototype._c = function(tagName) {
 	return document.createElement(tagName);
 };
 
-Board.prototype._imgMap = {
+FooBoard.prototype._imgMap = {
 	r: 'br', n: 'bn', b: 'bb', q: 'bq', k: 'bk', p: 'bp',
 	R: 'wr', N: 'wn', B: 'wb', Q: 'wq', K: 'wk', P: 'wp'
 };
 
 if (typeof exports != 'undefined') {
-	exports.Board = Board;
+	exports.FooBoard = FooBoard;
 }
