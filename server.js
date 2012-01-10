74f4161562535c6eeaefe50008f8a6382d2e6c10
Stig Murberg <stimurbe@cisco.com>
2012-01-11 00:04:13 +0100

    removed unused stuff

--- server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express.createServer();
-app.listen(8000);
-
-var bundle = require('browserify')(__dirname + '/src/kingside/kingside.js');
-app.use(bundle);
