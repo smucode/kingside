c1c4d76a7dcb0c6efe44606b8bc1341ec8936e6d
Stig Murberg <stimurbe@cisco.com>
2012-01-10 22:57:26 +0100

    wip: ui work

--- src/fooboard/fooboard.js
+++ src/fooboard/fooboard.js
@@ -1,111 +1,122 @@
 define(['underscore'], function(_) {
-	
-	var FooBoard = function(opts) {
-		opts = opts || {};
-		
-		this.squares = {};
-		this.target = opts.target;
-		this.pieces = opts.pieces || {};
-		
-		this.files = 'xabcdefgh'.split('');
-		this.ranks = '87654321x'.split('');
-		
-		this._create();
-		this.render();
-	};
-	
-	// public
-	
-	FooBoard.prototype.render = function(target) {
-		target = target || this.target;
-		if (target) {
-			target.appendChild(this.table);
-		}
-	};
-	
-	FooBoard.prototype.update = function(obj) {
-		_.each(obj.board, function(piece, pos) {
-			this._setImage(pos, piece);
-		}, this);
-	};
-	
-	// events
-	
-	FooBoard.prototype.onMove = function(from, to) {
-	};
-	
-	// private
-	
-	FooBoard.prototype._create = function() {
-		this.table = this._c('table');
-		this.table.className = 'foo-board';
-		_.each(this.ranks, function(rank) {
-			var tr = this._c('tr');
-			_.each(this.files, function(file) {
-				var td = this._createTd(file, rank);
-				if (file != 'x' || rank != 'x') {
-    				this._makeDroppable(td);
-				}
-				tr.appendChild(td);
-			}, this);
-			this.table.appendChild(tr);
-		}, this);
-	};
-	
-	FooBoard.prototype._makeDroppable = function(node) {
-	    var that = this;
-	    $(node).droppable({
-	        drop: function(event, ui) {
+
+    var FooBoard = function(opts) {
+        opts = opts || {};
+
+        this.squares = {};
+        this.target = opts.target;
+        this.pieces = opts.pieces || {};
+
+        this.files = 'xabcdefgh'.split('');
+        this.ranks = '87654321x'.split('');
+
+        this._create();
+        this.render();
+    };
+    
+    // public
+
+    FooBoard.prototype.render = function(target) {
+        target = target || this.target;
+        if(target) {
+            target.appendChild(this.table);
+        }
+    };
+
+    FooBoard.prototype.update = function(obj) {
+        _.each(obj.board, function(piece, pos) {
+            this._setImage(pos, piece);
+        }, this);
+        this.board = obj;
+    };
+    // private
+
+    FooBoard.prototype._create = function() {
+        this.table = this._c('table');
+        this.table.className = 'foo-board';
+        _.each(this.ranks, function(rank) {
+            var tr = this._c('tr');
+            _.each(this.files, function(file) {
+                var td = this._createTd(file, rank);
+                if(file != 'x' || rank != 'x') {
+                    this._makeDroppable(td);
+                }
+                tr.appendChild(td);
+            }, this);
+            this.table.appendChild(tr);
+        }, this);
+    };
+
+    FooBoard.prototype._makeDroppable = function(node) {
+        var that = this;
+        $(node).droppable({
+            drop : function(event, ui) {
                 var source = ui.draggable.parent().attr('_pos');
                 var target = $(this).attr('_pos');
-                
-                if (source != target) {
-    	            var img = ui.draggable.remove();
-    	            that._setImage(target, img.attr('_type'));
+
+                if(!_.include(that.board.valid_moves[source], target)) {
+                    return false;
                 }
-	        }
-	    });
-	};
 
-	FooBoard.prototype._createTd = function(file, rank) {
-		var td = this._c('td');
-		if (file == 'x' || rank == 'x') {
-			td.className = 'legend';
-			td.innerHTML = (file != 'x' ? file : (rank != 'x' ? rank : '')).toUpperCase();
-		} else {
-			td.setAttribute('_pos', file + rank);
-			td.className = this._getClassName(file, rank);
-			this.squares[file + rank] = td;
-		}
-		return td; 
-	};
+                $(that).trigger('onMove', [source, target]);
 
-	FooBoard.prototype._getClassName = function(file, rank) {
-		var fileIdx = (_.indexOf(this.files, file) % 2);
-		return fileIdx - (rank % 2) ? 'light' : 'dark';
-	};
-	
-	FooBoard.prototype._setImage = function(pos, piece) {
-		var td = this.squares[pos];
-		var type = this._imgMap[piece];
-		td.innerHTML = '<img src="img/' + type +'.png" _type=' + piece + ' />';
-		$(td).find('img').draggable({
-		    revert: true
-		});
-	};
+                if(source != target) {
+                    var img = ui.draggable.remove();
+                    that._setImage(target, img.attr('_type'));
+                }
+            }
+        });
+    };
 
-	FooBoard.prototype._attachEvents = function() {
-		$(this.table).find('img').draggable();
-	};
-	
-	FooBoard.prototype._c = function(tagName) {
-		return document.createElement(tagName);
-	};
-	
-	FooBoard.prototype._imgMap = {
-		r: 'br', n: 'bn', b: 'bb', q: 'bq', k: 'bk', p: 'bp',
-		R: 'wr', N: 'wn', B: 'wb', Q: 'wq', K: 'wk', P: 'wp'
-	};
-	
-	return FooBoard;
+    FooBoard.prototype._createTd = function(file, rank) {
+        var td = this._c('td');
+        if(file == 'x' || rank == 'x') {
+            td.className = 'legend';
+            td.innerHTML = (file != 'x' ? file : (rank != 'x' ? rank : '')).toUpperCase();
+        } else {
+            td.setAttribute('_pos', file + rank);
+            td.className = this._getClassName(file, rank);
+            this.squares[file + rank] = td;
+        }
+        return td;
+    };
+
+    FooBoard.prototype._getClassName = function(file, rank) {
+        var fileIdx = (_.indexOf(this.files, file) % 2);
+        return fileIdx - (rank % 2) ? 'light' : 'dark';
+    };
+
+    FooBoard.prototype._setImage = function(pos, piece) {
+        var td = this.squares[pos];
+        var type = this._imgMap[piece];
+        td.innerHTML = '<img src="img/' + type + '.png" _type=' + piece + ' />';
+        $(td).find('img').draggable({
+            revert : true
+        });
+    };
+
+    FooBoard.prototype._attachEvents = function() {
+        $(this.table).find('img').draggable();
+    };
+
+    FooBoard.prototype._c = function(tagName) {
+        return document.createElement(tagName);
+    };
+
+    FooBoard.prototype._imgMap = {
+        r : 'br',
+        n : 'bn',
+        b : 'bb',
+        q : 'bq',
+        k : 'bk',
+        p : 'bp',
+        R : 'wr',
+        N : 'wn',
+        B : 'wb',
+        Q : 'wq',
+        K : 'wk',
+        P : 'wp'
+    };
+
+    return FooBoard;
 });
--- src/kingside/kingside.js
+++ src/kingside/kingside.js
@@ -1,109 +1,142 @@
-define('kingside', ['src/fooboard/fooboard', 'src/rex/Board'], function(FooBoard, Rex) {
-	$(function() {
-	    
-		var fb = new FooBoard({
-			target: $('body')[0]
-		});
-		
-		var rex = new Rex.Board();
-		
-		fb.update({
-			board: rex._fen.pieces
-		});
-		
-	});
+define('kingside', ['underscore', 'src/fooboard/fooboard', 'src/rex/Board'], function(_, FooBoard, Rex) {
+    $(function() {
+
+        var fb = new FooBoard({
+            target : $('body')[0]
+        });
+
+        var rex = new Rex();
+
+        var gm = function() {
+            var m = {};
+            _.each(rex._getPieces(rex._getCurrentColor()), function(p) {
+                if(p.moves.length > 0) {
+                    var moves = _.map(p.moves, function(move) {
+                        return rex._idxToPos(move);
+                    });
+                    m[rex._idxToPos(p.idx)] = moves;
+                }
+            });
+            return m;
+        };
+        
+        var generateMove = function() {
+            var pieces = _.shuffle(rex._getPieces(rex._getCurrentColor()));
+            var piece = _.find(pieces, function(p) {
+                return p.moves.length > 0;
+            });
+            if(piece) {
+                var target = _.shuffle(piece.moves)[0];
+                var move = _.map([piece.idx, target], rex._idxToPos, rex);
+                rex.move.apply(rex, move);
+                updateFB();
+            }
+        };
+        var updateFB = function() {
+            fb.update({
+                board : rex._fen.pieces,
+                valid_moves : gm()
+            });
+        };
+        updateFB();
+
+        $(fb).bind('onMove', function(x, source, target) {
+            rex.move(source, target);
+            updateFB();
+            generateMove();
+        });
+    });
 });
 
 
-
 // $.domReady(function() {
-// 	
-	// // todo: add support for require.js (elns)
-	// // desperately need some sort of package system
-// 	
-	// var rex = require('./Board');
-// 	
-	// var ks = {};
-// 	
-	// // Board
-// 	
-	// ks.Board = function() {
-		// this.rex = new rex.Board();
-		// var fb = new FooBoard(this.rex._fen.pieces);
-		// fb.render(document.getElementsByTagName('body')[0]);
-// 		
-		// bean.add(this, 'onMove', function(status) {
-			// fb.move(status.from, status.to);
-		// });
-// 		
-		// var getAn = function(img) {
-			// return img.parentNode.getAttribute('xan');
-		// };
-// 		
-		// var that = this;
-		// var source = null;
-// 		
-		// $('img').dagron({
-			// 'target': 'img',
-			// 'start': function(img) {
-				// var an = getAn(img);
-				// source = an;
-			// },
-			// 'drop': function(img) {
-				// var an = getAn(img);
-				// that.move(source, an);
-				// // b.move(source, an);
-				// // fb.move(source, an);
-			// }
-		// });
-	// };
-// 	
-	// ks.Board.prototype.move = function(from, to) {
-		// try {
-			// var status = this.rex.move(from, to);
-			// bean.fire(this, 'onMove', status);
-		// } catch (err) {
-			// console.error(err)
-			// bean.fire(this, 'onError', err);
-		// }
-	// };
-// 	
-	// // Computer
-// 	
-	// ks.Noobolainen = function(board) {
-// 		
-		// var b = board.rex;
-// 		
-		// bean.add(board, 'onMove', function(status) {
-			// if (!status.finished && status.active == 'b') {
-				// generateMove();
-			// }
-		// });
-// 		
-		// var generateMove = function() {
-			// var pieces = __.shuffle(b._getPieces(b._getCurrentColor()));
-			// var piece = __.find(pieces, function(p) {
-				// return p.moves.length > 0;
-			// });
-			// if (piece) {
-				// var target = __.shuffle(piece.moves)[0];
-// 				
-				// var move = __.map([piece.idx, target], b._idxToPos, b);
-				// var x = b._getPieceAt(target);
-				// if (x && x.type == 3) {
-					// console.warn(move);
-					// return;
-				// }
-// 				
-				// console.log(b._fen.fullmove, move);
-				// board.move.apply(board, move);
-			// } else {
-				// alert('wtf!');
-			// }
-		// };
-	// };
-// 	
-	// var board = new ks.Board();
-	// var cpu = new ks.Noobolainen(board);
-// 	
+//
+// // todo: add support for require.js (elns)
+// // desperately need some sort of package system
+//
+// var rex = require('./Board');
+//
+// var ks = {};
+//
+// // Board
+//
+// ks.Board = function() {
+// this.rex = new rex.Board();
+// var fb = new FooBoard(this.rex._fen.pieces);
+// fb.render(document.getElementsByTagName('body')[0]);
+//
+// bean.add(this, 'onMove', function(status) {
+// fb.move(status.from, status.to);
+// });
+//
+// var getAn = function(img) {
+// return img.parentNode.getAttribute('xan');
+// };
+//
+// var that = this;
+// var source = null;
+//
+// $('img').dagron({
+// 'target': 'img',
+// 'start': function(img) {
+// var an = getAn(img);
+// source = an;
+// },
+// 'drop': function(img) {
+// var an = getAn(img);
+// that.move(source, an);
+// // b.move(source, an);
+// // fb.move(source, an);
+// }
+// });
+// };
+//
+// ks.Board.prototype.move = function(from, to) {
+// try {
+// var status = this.rex.move(from, to);
+// bean.fire(this, 'onMove', status);
+// } catch (err) {
+// console.error(err)
+// bean.fire(this, 'onError', err);
+// }
+// };
+//
+// // Computer
+//
+// ks.Noobolainen = function(board) {
+//
+// var b = board.rex;
+//
+// bean.add(board, 'onMove', function(status) {
+// if (!status.finished && status.active == 'b') {
+// generateMove();
+// }
+// });
+//
+// var generateMove = function() {
+// var pieces = __.shuffle(b._getPieces(b._getCurrentColor()));
+// var piece = __.find(pieces, function(p) {
+// return p.moves.length > 0;
+// });
+// if (piece) {
+// var target = __.shuffle(piece.moves)[0];
+//
+// var move = __.map([piece.idx, target], b._idxToPos, b);
+// var x = b._getPieceAt(target);
+// if (x && x.type == 3) {
+// console.warn(move);
+// return;
+// }
+//
+// console.log(b._fen.fullmove, move);
+// board.move.apply(board, move);
+// } else {
+// alert('wtf!');
+// }
+// };
+// };
+//
+// var board = new ks.Board();
+// var cpu = new ks.Noobolainen(board);
+//
 // });
