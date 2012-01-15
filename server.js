d4e34af44d3cf947ef3b2c0f4bd0e7a77ad8a93c
Jon Tore Hafstad <jontore@gmail.com>
2012-01-15 20:11:26 +0100

    WIP: working on outh authentication with google

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 var connect = require('connect');
+var everyauth = require('everyauth');
+
+everyauth.googlehybrid
+    .myHostname('http://kingsi.de:8000')
+    .consumerKey('kingsi.de')
+    .consumerSecret('cJ_R8LWwNLwV6z71S-OD3wam')
+    .scope(['https://www.googleapis.com/oauth2/v1/userinfo'])
+    .findOrCreateUser( function (session, userAttributes) {
+    })
+    .moduleErrback( function (err) {
+        console.log('Failed', err);
+    })
+.redirectPath('/');
 
 var port = process.env.PORT || 8000;
 
 console.log('starting kingside on port ' + port);
 
 connect(
-    connect.static(__dirname + '/build')
+    connect.static(__dirname + '/build'),
+    connect.bodyParser(),
+    connect.cookieParser(),
+    connect.session({secret: 'whodunnit'}),
+    everyauth.middleware()
 ).listen(
-    port
+    process.env.PORT || 8000
 );
