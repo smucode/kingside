d884f1065d1cd90f537385fd30e75dce2e6f13ec
Jon Tore Hafstad <jontore@gmail.com>
2012-01-16 23:35:24 +0100

    Wip, testing out dnode

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
 var connect = require('connect');
 var everyauth = require('everyauth');
+var dnode = require('dnode');
 
+var port = process.env.PORT || 8000;
+
+var userInfo = {
+    user: 'No one'
+};
+
+//google authorization
 everyauth.googlehybrid
     .myHostname('http://kingsi.de:8000')
     .consumerKey('kingsi.de')
     .consumerSecret('cJ_R8LWwNLwV6z71S-OD3wam')
     .scope(['https://www.googleapis.com/auth/userinfo.profile'])
     .findOrCreateUser( function (session, userAttributes) {
-        console.error('user', session, userAttributes);
+        userInfo = userAttributes;
         return userAttributes.claimedIdentifier;
     })
-.redirectPath('/');
-
-var port = process.env.PORT || 8000;
+.redirectPath('/')
+.entryPath('/auth/google')
+.callbackPath('/auth/google/callback');
 
 console.log('starting kingside on port ' + port);
 
-connect(
-    connect.static(__dirname + '/build'),
+var server = connect(
+    connect.static(__dirname),
     connect.bodyParser(),
     connect.cookieParser(),
     connect.session({secret: 'whodunnit'}),
@@ -25,3 +33,11 @@ connect(
 ).listen(
     process.env.PORT || 8000
 );
+    
+//dnode 
+var dnode_server = dnode({
+    user : function(cb) { 
+        cb(userInfo);
+    }
+}).listen(3000);
+
--- src/kingside/kingside.js
+++ src/kingside/kingside.js
@@ -1,8 +1,17 @@
-define('kingside', ['underscore', 'src/fooboard/fooboard', 'src/rex/Board', 'src/garbo/garbo', 'src/event/event', 'src/kingside/status'], function(_, FooBoard, Rex, Garbo, Event, Status) {
+define('kingside', ['underscore', 'src/fooboard/fooboard', 'src/rex/Board', 'src/garbo/garbo',
+        'src/event/event', 'src/kingside/status'], function(_, Dnode, FooBoard, Rex, Garbo, Event, Status) {
     $(function() {
         var target = $('.content')[0];
 
         var rex = new Rex();
+
+        var dnode = require('node_modules/dnode/browser/bundle.js');
+
+        dnode.connect(3000, function(remote) {
+            remote.user(function(info){
+                console.error('user info', info);
+            });
+        });
         
         var status = new Status({ target: target });
         rex.onMove(_.bind(status.update, status));
