b3d8caec71caef7fe4fdac704f768c0cdf7c079c
Stig Murberg <stimurbe@cisco.com>
2012-01-18 23:51:35 +0100

    socket.io work from cafekontoret, threw in a little cleanup as well

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+var _ = require('underscore');
+var io = require('socket.io');
 var connect = require('connect');
 var everyauth = require('everyauth');
-var io = require('socket.io');
-var RedisStore = require('connect-redis')(connect);
 
 var port = process.env.PORT || 8000;
 
 var userInfo = {};
+
 //google authorization
 everyauth.googlehybrid
     .myHostname('http://kingsi.de:8000')
@@ -16,9 +17,9 @@ everyauth.googlehybrid
         userInfo = userAttributes;
         return userAttributes.claimedIdentifier;
     })
-.redirectPath('/')
-.entryPath('/auth/google')
-.callbackPath('/auth/google/callback');
+    .redirectPath('/')
+    .entryPath('/auth/google')
+    .callbackPath('/auth/google/callback');
 
 console.log('starting kingside on port ' + port);
 
@@ -26,16 +27,33 @@ var server = connect(
     connect.static(__dirname),
     connect.bodyParser(),
     connect.cookieParser(),
-    connect.session({secret: 'whodunnit'}),
-    everyauth.middleware(),
-    connect.cookieParser(),
-    connect.session({ store: new RedisStore, secret: 'tacocat is a palindrome' })
+    connect.session({secret: 'secret', key: 'express.sid'}),
+    everyauth.middleware()
 );
 
-var io = require('socket.io').listen(80);
+var io = require('socket.io').listen(server);
+io.set('log level', 1);
+
+var parseCookie = function(cookies) {
+    var s = {};
+    _.each(cookies.split(';\ '), function(c) {
+        var vals = c.split('=');
+         s[vals[0]] = vals[1];
+    });
+    return s;
+};
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('auth', userInfo);
+    console.log('SID: ' + socket.handshake.sessionID);
+    socket.emit('auth', userInfo);
+});
+
+io.set('authorization', function (data, accept) {
+    if (data.headers.cookie) {
+        data.cookie = parseCookie(data.headers.cookie);
+        data.sessionID = data.cookie['express.sid'];
+    }
+    accept(null, true);
 });
 
 server.listen(port);
--- src/kingside/auth.js
+++ src/kingside/auth.js
@@ -3,7 +3,18 @@ define('socket.io', function() {
 });
 
 define(['underscore', 'socket.io'], function(_, io) {
+    
+    var parseCookie = function(cookies) {
+        var s = {};
+        _.each(cookies.split(';\ '), function(c) {
+            var vals = c.split('=');
+             s[vals[0]] = vals[1];
+        });
+        return s;
+    };
+    
     return function() {
+        console.log(document.cookie);
         var socket = io.connect('http://localhost');
         socket.on('auth', function (userinfo) {
             console.log('user', userinfo);
