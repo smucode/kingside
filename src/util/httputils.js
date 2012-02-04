fba0af7e4fa9335d008579d7dd41421f1384a949
Stig Murberg <stimurbe@cisco.com>
2012-02-04 20:20:15 +0100

    fixed httputils if headers not set, updated title and noscript

--- src/util/httputils.js
+++ src/util/httputils.js
@@ -2,9 +2,11 @@ var _ = require('underscore');
 
 exports.parseCookie = function(cookie) {
     var s = {};
-    _.each(cookie.split(';\ '), function(c) {
-        var vals = c.split('=');
-         s[vals[0]] = vals[1];
-    });
+    if (cookie) {
+        _.each(cookie.split(';\ '), function(c) {
+            var vals = c.split('=');
+             s[vals[0]] = vals[1];
+        });
+    }
     return s;
 };
