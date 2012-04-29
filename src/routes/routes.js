1f8028721c7882228cfa45d5b6a2b167a0682eb2
Jon Tore Hafstad <jontore@gmail.com>
2012-04-29 14:13:05 +0200

    Created a game routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,8 +2,8 @@ var util = require('../util/httputils');
 var cache = require('../cache/userCache').UserCache;
 var gameService = require('../services/gameService').GameService;
 var userRoutes = require('./userRoutes').UserRoutes;
+var gameRoutes = require('./gameRoutes').GameRoutes;
 var buddyRoutes = require('./buddyRoutes').BuddyRoutes;
-var buddyService = require('../services/buddyService').BuddyService;
 
 var Routes = function() {
 };
@@ -17,22 +17,7 @@ Routes.prototype.get = function() {
     return {
         '/user': userRoutes.get,
         '/buddies': buddyRoutes.get,
-        '/games': function(req, res, next){
-            try {
-                var user = getUser(req);
-                res.contentType('json');
-                if(user) {
-                    gameService.findUserGames(user.email, function(games) {
-                        var json = games ? JSON.stringify(games) : '';
-                        res.send(json);
-                    });
-                } else {
-                    res.send('{}');
-                }
-            } catch (e) {
-                res.send(e);
-            }
-        },
+        '/games': gameRoutes.get,
         '/request_game/': function(req, res, next){
             var user = getUser(req);
             res.contentType('json');
@@ -44,19 +29,19 @@ Routes.prototype.get = function() {
                 res.send('');
             }
         }
-    }
+    };
 };
 
 Routes.prototype.put = function() {
     return {
         '/buddies': buddyRoutes.put
-    }
+    };
 };
     
 Routes.prototype.del = function() {
     return {
         '/buddies': buddyRoutes.del
-    }
+    };
 };
 
 exports.Routes = new Routes();
